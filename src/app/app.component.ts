aff81078d03113d8ca44fb6344cf1fae7d710629
davidemarcoli <davide@marcoli.ch>
2022-09-29 21:51:36 +0200

    Change images to .webp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 // @ts-ignore
-import Typewriter from 'typewriter-effect/dist/core';
-// @ts-ignore
 import AOS from "aos";
 
 @Component({
@@ -14,27 +12,5 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     AOS.init();
-
-    let target = document.querySelector("#writer")
-
-    const typewriter = new Typewriter(target, {
-      loop: true,
-      delay: 75,
-    });
-
-    typewriter
-      .pauseFor(1000)
-      .typeString('I\'m a')
-      .pauseFor(250)
-      .typeString('n <strong><span><a href="https://angular.io" style="text-decoration: none; color: firebrick">Angular</a></span></strong> Dev')
-      .pauseFor(300)
-      .deleteChars(14)
-      .typeString(' <strong><span><a href="https://spring.io" style="text-decoration: none; color: darkgreen">Spring</a></span></strong> Dev')
-      .pauseFor(300)
-      .deleteChars(11)
-      .typeString(' <strong><span style="color: darkviolet">Student</span></strong>')
-      // .pauseFor(300)
-      .deleteChars(13)
-      .start();
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import Typewriter from 'typewriter-effect/dist/core';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,27 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let target = document.querySelector("#writer")
+
+    const typewriter = new Typewriter(target, {
+      loop: true,
+      delay: 75,
+    });
+
+    typewriter
+      .pauseFor(1000)
+      .typeString('I\'m a')
+      .pauseFor(250)
+      .typeString('n <strong><span><a href="https://angular.io" style="text-decoration: none; color: rgba(255,50,50,1)">Angular</a></span></strong> Dev')
+      .pauseFor(300)
+      .deleteChars(14)
+      .typeString(' <strong><span><a href="https://spring.io" style="text-decoration: none; color: darkgreen">Spring</a></span></strong> Dev')
+      .pauseFor(300)
+      .deleteChars(11)
+      .typeString(' <strong><span style="color: darkviolet">Student</span></strong>')
+      // .pauseFor(300)
+      .deleteChars(13)
+      .start();
   }
 
 }
